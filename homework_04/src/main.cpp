147b8a66dd573e0def914406da2f91af632e280c
silvershadow82 <cjshadow@gmail.com>
2026-05-07 14:00:59 +0000

    Removed structs and unnecessary file pre-reads

--- homework_04/src/main.cpp
+++ homework_04/src/main.cpp
@@ -2,37 +2,10 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
-#include <ostream>
-#include <string>
 
 const int ticker_per_revolution = 1024;
 const float wheel_radius_m = 0.3;
 const float wheel_base_m = 1.0;
-
-struct WheelMetrics {
-  long timestamp_ms;
-  long fl_ticks;
-  long fr_ticks;
-  long bl_ticks;
-  long br_ticks;
-};
-
-int lineCount(const char* fileName)
-{
-  std::ifstream input(fileName);
-  if (!input.is_open()) {
-    return -1;
-  }
-  std::string line;
-  int lineCount{0};
-
-  while (std::getline(input, line)) {
-    lineCount++;
-  }
-
-  input.close();
-  return lineCount;
-}
 
 int main(int argc, char** argv)
 {
@@ -43,11 +16,7 @@ int main(int argc, char** argv)
   }
 
   const char* inputFileName{argv[1]};
-  const int lines{lineCount(inputFileName)};
 
-  // std::cout << "Counted " << lines << " lines in " << inputFileName << std::endl;
-
-  // Reopen file for actual reading
   std::ifstream inputFile(inputFileName);
 
   if (!inputFile.is_open()) {
@@ -55,34 +24,31 @@ int main(int argc, char** argv)
     return 1;
   }
 
-  if (lines <= 0) {
-    std::cerr << "Invalid input file data!" << std::endl;
-    return 1;
-  }
-
-  // Read into struct
-  WheelMetrics* metrics = new WheelMetrics[lines];
-
-  for (int i = 0; i < lines; i++) {
-    metrics[i] = WheelMetrics{};
-    inputFile >> metrics[i].timestamp_ms >> metrics[i].fl_ticks >> metrics[i].fr_ticks >> metrics[i].bl_ticks >> metrics[i].br_ticks;
-  }
-
-  // Close early
-  inputFile.close();
-
+  long timestamp_ms{0}, fl_ticks{0}, fr_ticks{0}, bl_ticks{0}, br_ticks{0}, prev_fl_ticks{0}, prev_fr_ticks{0}, prev_bl_ticks{0},
+    prev_br_ticks{0};
   float d_fl{0.f}, d_fr{0.f}, d_bl{0.f}, d_br{0.f}, d_left{0.f}, d_right{0.f}, distance_per_tick{0.f}, dL{0.f}, dR{0.f}, d{0.f},
     dthetha{0.f}, x{0.f}, y{0.f}, theta{0.f};
 
   // Now main cycle
-  for (int i = 1; i < lines; i++) {
-    d_fl = metrics[i].fl_ticks - metrics[i - 1].fl_ticks;
-    d_fr = metrics[i].fr_ticks - metrics[i - 1].fr_ticks;
-    d_bl = metrics[i].bl_ticks - metrics[i - 1].bl_ticks;
-    d_br = metrics[i].br_ticks - metrics[i - 1].br_ticks;
+  for (int i = 0; !inputFile.eof(); i++) {
+    inputFile >> timestamp_ms >> fl_ticks >> fr_ticks >> bl_ticks >> br_ticks;
+
+    if (i == 0) {
+      prev_fl_ticks = fl_ticks;
+      prev_fr_ticks = fr_ticks;
+      prev_bl_ticks = bl_ticks;
+      prev_br_ticks = br_ticks;
+      continue;
+    }
+
+    d_fl = fl_ticks - prev_fl_ticks;
+    d_fr = fr_ticks - prev_fr_ticks;
+    d_bl = bl_ticks - prev_bl_ticks;
+    d_br = br_ticks - prev_br_ticks;
 
     d_left = (d_fl + d_bl) / 2;
     d_right = (d_fr + d_br) / 2;
+
     distance_per_tick = 2 * M_PI * wheel_radius_m / ticker_per_revolution;
 
     dL = d_left * distance_per_tick;
@@ -94,13 +60,19 @@ int main(int argc, char** argv)
     x += d * cos(theta + dthetha / 2);
     y += d * sin(theta + dthetha / 2);
 
-    theta += dthetha;
     // Output data
-    std::cout << metrics[i].timestamp_ms << " " << x << " " << y << " " << theta << std::endl;
+    std::cout << timestamp_ms << " " << x << " " << y << " " << theta << std::endl;
+
+    prev_fl_ticks = fl_ticks;
+    prev_fr_ticks = fr_ticks;
+    prev_bl_ticks = bl_ticks;
+    prev_br_ticks = br_ticks;
+
+    theta += dthetha;
   }
 
-  // Cleanup
-  delete[] metrics;
+  // Close the file
+  inputFile.close();
 
   return 0;
 }

83d7be639d4c08aed4776b780ca2a3ba5c3eb148
Ruslan Shtyk <cjshadow@gmail.com>
2026-07-01 19:45:26 +0000

    chore: cleanup 1

--- homework_08/include/interfaces/ITargetProvider.h
+++ homework_08/include/interfaces/ITargetProvider.h
@@ -1,11 +1,10 @@
 #pragma once
 
 #include "models/Target.h"
-#include <string>
 
 class ITargetProvider {
 public:
   virtual int getTargetCount() = 0;
   virtual Target getTarget(int index) = 0;
-  virtual ~ITargetProvider() {};
+  virtual ~ITargetProvider(){};
 };
